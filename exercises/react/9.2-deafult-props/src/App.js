6dcda80ed812638eaf092903951a7e6eed721366
ChenHzt <chenhzt@gmail.com>
2021-03-16 16:16:37 +0200

    exercises 16.3.2021

---
+++ exercises/react/9.2-deafult-props/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class MusicAlbum extends React.Component{
+  constructor(props){
+    super(props);
+    // let title,artist,numOfSongs,length,link; 
+    const {title,artist,numOfSongs,length,link} = props;
+    this.state = {
+      
+    }
+  }
+}
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
